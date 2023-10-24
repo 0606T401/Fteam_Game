cd4781e9c20b0159e0b3a7a64a38e5e1d5717aa0
rio123o <113337983+rio123o@users.noreply.github.com>
2023-10-24 15:50:24 +0900

    afterOneSecond

--- src/Assets/Script/StartTimer.cs
+++ src/Assets/Script/StartTimer.cs
@@ -20,10 +20,21 @@ public class StartTimer : MonoBehaviour
         {
             CountDownTimer.text = string.Format("{0:0}", CountDownTime);
         }
-        if (CountDownTime < 0) 
+        if (CountDownTime < 0 && CountDownTime >= -1.0f) 
         {
             CountDownTimer.text = string.Format("GO!");
+            StartCoroutine("after_one_second");
+            
         }
         CountDownTime -= Time.deltaTime;
+    }
+
+
+
+
+    IEnumerator after_one_second()
+    {
+        yield return new WaitForSeconds(1.0f);
+        CountDownTimer.text = string.Format("");
     }
 }
