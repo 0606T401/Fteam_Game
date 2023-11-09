995ce594a15a08fec6d775c2f2b5d9489434c1a1
Hayato222 <g2227070@st.t-kougei.ac.jp>
2023-11-09 16:38:58 +0900

    穴のサイズ1.5倍

--- src/Assets/Script/Acceletion.cs
+++ src/Assets/Script/Acceletion.cs
@@ -7,8 +7,12 @@ public class Acceletion : MonoBehaviour
     private HeadPoint head;
     private FeetPoint feet;
     private ArmPoint arm;
+    private JustHeadPoint justHead;
+    private JustArmPoint justArm;
+    private JustFeetPoint justFeet;
 
-    public float acceleration = 5f;
+    public float acceleration = 2.5f;
+    public float justAcceleration = 5f;
     public Data data;
     // Start is called before the first frame update
     void Start()
@@ -16,11 +20,25 @@ public class Acceletion : MonoBehaviour
         head = GameObject.Find("HeadPoint").GetComponent<HeadPoint>();
         feet = GameObject.Find("FeetPoint").GetComponent<FeetPoint>();
         arm = GameObject.Find("ArmPoint").GetComponent<ArmPoint>();
+        justHead = GameObject.Find("JustHeadPoint").GetComponent<JustHeadPoint>();
+        justFeet = GameObject.Find("JustFeetPoint").GetComponent<JustFeetPoint>();
+        justArm = GameObject.Find("JustArmPoint").GetComponent<JustArmPoint>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(justArm.JustPassArm&&justFeet.JustPassFeet&&justHead.JustPassHead)
+        {
+            Debug.Log("JustPass");
+
+            data.fallspeed += justAcceleration;
+
+            justArm.JustPassArm = false;
+            justFeet.JustPassFeet = false;
+            justHead.JustPassHead = false;
+        }
+
         if(arm.PassArm&&feet.PassFeet&&head.PassHead)
         {
             Debug.Log("Pass");
