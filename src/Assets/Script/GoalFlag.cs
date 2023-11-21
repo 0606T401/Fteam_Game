cd32d71f0f21c195a7206f71ecbd63056a7828b6
rio123o <113337983+rio123o@users.noreply.github.com>
2023-11-21 16:55:50 +0900

    Update GoalFlag.cs

--- src/Assets/Script/GoalFlag.cs
+++ src/Assets/Script/GoalFlag.cs
@@ -30,10 +30,12 @@ public class GoalFlag : MonoBehaviour
                 Goal = true;
                 Debug.Log("Goal ok");
                 data.fallspeed = 0f;
-                if(GameOver == false)
-                EventSystem.current.SetSelectedGameObject(null);
-                EventSystem.current.SetSelectedGameObject(PauseFirstButton);
-                ClearPanel.SetActive(true);
+                if (GameOver == false)
+                {
+                    EventSystem.current.SetSelectedGameObject(null);
+                    EventSystem.current.SetSelectedGameObject(PauseFirstButton);
+                    ClearPanel.SetActive(true);
+                }
             }
             
         }
