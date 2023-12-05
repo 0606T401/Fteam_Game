9baa9ad103cefbda440e5e50566f551ae33ccbab
rio123o <113337983+rio123o@users.noreply.github.com>
2023-12-05 11:44:41 +0900

    Imput_Changes

--- src/Assets/Script/GamepadInputSys.cs
+++ src/Assets/Script/GamepadInputSys.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class stickInputSystem : MonoBehaviour
+public class GamepadInputSys : MonoBehaviour
 {
     // InputActionAssetÇ÷ÇÃéQè∆
     [SerializeField] private InputActionReference _moveAction;
